8ada4d1a0d0874db49676c6c927dd0135f0b4f10
faizanb <faizanb@umich.edu>
2022-01-30 14:35:46 -0500

    added footer design

--- footer.php
+++ footer.php
@@ -2,13 +2,24 @@
 
 <div id="bottom-panel">
     <div>
-        Footer left
+        <img id="footer-logo" src="<?php echo get_template_directory_uri(); ?>/images/white-logo.png" />
     </div>
     <div>
-        Footer center
+        <p id="home-footer-tab"> Home </p> <p id="middle-footer-section">Services <br> Volunteer <br> About </p>
     </div>
-    <div>
-        Footer right
+    <div id = "footer-rectangle">
+        <a href = "https://www.facebook.com/teddybeartravel/" class = "social-account">  
+            <ion-icon id = "facebook-icon" name="logo-facebook"></ion-icon>
+            <div id = "facebook-text">AAT Facebook</div>
+        </a>
+        <a  href="tel:5867574177" class = "social-account">  
+            <ion-icon id = "phone-icon" name="call-outline"></ion-icon>
+            <div id = "phone-text">586-757-4177</div>
+        </a>
+        <a href="mailto:AAT@gmail.com" class = "social-account">  
+            <ion-icon id = "mail-icon" name="mail-outline"></ion-icon>
+            <div id = "facebook-text">AAT@gmail.com</div>
+        </a>
     </div>
 </div>
 

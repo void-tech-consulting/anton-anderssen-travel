1d96990780328f42c57ab33b6e6275a4df385f48
faizanb <faizanb@umich.edu>
2022-01-30 15:01:52 -0500

    added company name to footer and centered elements

--- footer.php
+++ footer.php
@@ -1,28 +1,26 @@
 <?php wp_footer(); ?>
-
-<div id="bottom-panel">
-    <div>
+    <div id="bottom-panel">
         <img id="footer-logo" src="<?php echo get_template_directory_uri(); ?>/images/white-logo.png" />
+        <div id = "footer-text">
+            <p id="home-footer-tab"> Home </p> <p id="middle-footer-section">Services <br> Volunteer <br> About </p>
+        </div>
+        <div id = "footer-rectangle">
+            <a href = "https://www.facebook.com/teddybeartravel/" class = "social-account">  
+                <ion-icon id = "facebook-icon" name="logo-facebook"></ion-icon>
+                <div id = "facebook-text">AAT Facebook</div>
+            </a>
+            <a  href="tel:5867574177" class = "social-account">  
+                <ion-icon id = "phone-icon" name="call-outline"></ion-icon>
+                <div id = "phone-text">586-757-4177</div>
+            </a>
+            <a href="mailto:AAT@gmail.com" class = "social-account">  
+                <ion-icon id = "mail-icon" name="mail-outline"></ion-icon>
+                <div id = "facebook-text">AAT@gmail.com</div>
+            </a>
+        </div>
     </div>
-    <div>
-        <p id="home-footer-tab"> Home </p> <p id="middle-footer-section">Services <br> Volunteer <br> About </p>
-    </div>
-    <div id = "footer-rectangle">
-        <a href = "https://www.facebook.com/teddybeartravel/" class = "social-account">  
-            <ion-icon id = "facebook-icon" name="logo-facebook"></ion-icon>
-            <div id = "facebook-text">AAT Facebook</div>
-        </a>
-        <a  href="tel:5867574177" class = "social-account">  
-            <ion-icon id = "phone-icon" name="call-outline"></ion-icon>
-            <div id = "phone-text">586-757-4177</div>
-        </a>
-        <a href="mailto:AAT@gmail.com" class = "social-account">  
-            <ion-icon id = "mail-icon" name="mail-outline"></ion-icon>
-            <div id = "facebook-text">AAT@gmail.com</div>
-        </a>
-    </div>
-</div>
 
+    <div id = "company-footer-title"> Anton Anderssen Travel </div>
 </body>
 
 </html>

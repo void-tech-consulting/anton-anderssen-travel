4ff03e4e7c3cc72d5c4019ed9418f3d22e23fdf8
celinatp942 <celinatp@umich.edu>
2022-05-02 19:34:05 +0800

    Added 4 service boxes
    The service boxes for Cruises, Flights, Resorts, and Hospice have been to the services page.

--- page-services.php
+++ page-services.php
@@ -1,10 +1,52 @@
 <?php get_header(); ?>
 
-<div id="page-content">
+<div class="page-content">
 
     <!-- Page content -->
-    <div id="page-center">
-        SERVICES PAGES!!!!!!!
+    <div class="page-center">
+        <div class="flex-row w-100 "> 
+            <div class="flex-column w-100"> 
+                <div style="background: transparent;" class= "service_box">
+                    <div style = "background: Lavender" class ="image_box w-50">
+                        <img class="image" src="<?php echo get_template_directory_uri(); ?>/images/cruise-holiday 1.png" />
+                        <div class="centered"> Cruises </div> 
+                    </div>
+                    <div style = "background: white" class = "text_box w-50">  
+                        <div class = "service_title"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dui purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus ut metus lectus. </div> 
+                    </div>
+                </div>
+
+                <div style="background: transparent;" class= "service_box">
+                    <div style = "background: Lavender" class ="image_box w-50">
+                        <img class="image" src="<?php echo get_template_directory_uri(); ?>/images/flight-getty 1.png" />
+                        <div class="centered"> Flights </div> 
+                    </div>
+                    <div style = "background: white" class = "text_box w-50">  
+                        <div class = "service_title"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dui purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus ut metus lectus. </div> 
+                    </div>
+                </div>
+
+                <div style="background: transparent;" class= "service_box">
+                    <div style = "background: Lavender" class ="image_box w-50">
+                        <img class="image" src="<?php echo get_template_directory_uri(); ?>/images/Beaches-Turks-Caicos-Overview 1.png" />
+                        <div class="centered"> Resorts </div> 
+                    </div>
+                    <div style = "background: white" class = "text_box w-50">  
+                        <div class = "service_title"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dui purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus ut metus lectus. </div> 
+                    </div>
+                </div>
+
+                <div style="background: transparent;" class= "service_box">
+                    <div style = "background: Lavender" class ="image_box w-50">
+                        <img class="image" src="<?php echo get_template_directory_uri(); ?>/images/Beaches-Turks-Caicos-Overview 2.png" />
+                        <div class="centered"> Hospice </div> 
+                    </div>
+                    <div style = "background: white" class = "text_box w-50">  
+                        <div class = "service_title"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dui purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus ut metus lectus. </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
